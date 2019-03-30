d45b514d6e5646ddf91ca1bcb36b314c438cb1ef
MargarytaChepiga <chepigamargaryta@gmail.com>
2019-03-30 17:33:51 -0400

    Fixed minor things

--- Project3/Project1/DetailViewController.swift
+++ Project3/Project1/DetailViewController.swift
@@ -21,6 +21,7 @@ class DetailViewController: UIViewController {
         
         title = "Picture \(selectedImageCount + 1) of \(totalAmountOfImages)"
         
+        // 
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .  action, target: self, action: #selector(shareTapped))
 
         navigationItem.largeTitleDisplayMode = .never
@@ -53,15 +54,5 @@ class DetailViewController: UIViewController {
         vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
         present(vc, animated: true)
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
