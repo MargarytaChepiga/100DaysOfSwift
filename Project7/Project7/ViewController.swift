64289956e62b6816c9c5d7a86f2b38bd47b0c2f0
MargarytaChepiga <chepigamargaryta@gmail.com>
2019-04-23 15:04:54 -0400

    Added Project 7 with basic user interface

---
+++ Project7/Project7/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by margaret on 2019-04-23.
+//  Copyright © 2019 margaret. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var petitions = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return petitions.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "Title goes here"
+        cell.detailTextLabel?.text = "Subtitle goes here"
+        return cell
+    }
+}
+
