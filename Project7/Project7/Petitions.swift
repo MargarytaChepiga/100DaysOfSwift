9f21638285c77b8eab565f79501e626cf63f50ab
MargarytaChepiga <chepigamargaryta@gmail.com>
2019-04-23 21:39:51 -0400

    Updated

---
+++ Project7/Project7/Petitions.swift
@@ -0,0 +1,13 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by margaret on 2019-04-23.
+//  Copyright © 2019 margaret. All rights reserved.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
--- Project7/Project7/ViewController.swift
+++ Project7/Project7/ViewController.swift
@@ -9,23 +9,44 @@
 import UIKit
 
 class ViewController: UITableViewController {
-    
-    var petitions = [String]()
 
+    var petitions = [Petition]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url) {
+                // we're OK to parse!
+                parse(json: data)
+                
+            }
+        }
     }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         return petitions.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = "Title goes here"
-        cell.detailTextLabel?.text = "Subtitle goes here"
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let petition = petitions[indexPath.row]
+        cell.textLabel?.text = petition.title
+        cell.detailTextLabel?.text = petition.body
         return cell
     }
+    
+    func parse(json: Data) {
+        let decoder = JSONDecoder()
+        
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+            petitions = jsonPetitions.results
+            tableView.reloadData()
+        }
+    }
+    
 }
 
