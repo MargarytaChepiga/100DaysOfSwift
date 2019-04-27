203f55889572e229586ba20140c3bfa1be8909bc
MargarytaChepiga <chepigamargaryta@gmail.com>
2019-04-26 20:41:01 -0400

    Added initial Project 8

---
+++ Project8/Project8/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Project8
+//
+//  Created by margaret on 2019-04-26.
+//  Copyright © 2019 margaret. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var cluesLabel: UILabel!
+    var answersLabel: UILabel!
+    var correctAnswer: UITextField!
+    var scoreLabel: UILabel!
+    var letterButtons = [UIButton]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
