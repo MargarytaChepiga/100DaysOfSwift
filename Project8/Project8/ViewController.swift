bf3a36b463dcac15d749d52a565a41c722a024e5
MargarytaChepiga <chepigamargaryta@gmail.com>
2019-04-26 21:08:33 -0400

    Added loadView function

--- Project8/Project8/ViewController.swift
+++ Project8/Project8/ViewController.swift
@@ -16,6 +16,27 @@ class ViewController: UIViewController {
     var scoreLabel: UILabel!
     var letterButtons = [UIButton]()
     
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .white
+        
+        scoreLabel = UILabel()
+        // make constrains by hand
+        scoreLabel.translatesAutoresizingMaskIntoConstraints = false
+        scoreLabel.textAlignment = .right
+        scoreLabel.text = "Score: 0"
+        view.addSubview(scoreLabel)
+        
+        // get all the constraint and activate them all at once
+        NSLayoutConstraint.activate([
+            // layoutmarginsGuide is used to add a distance from the edge of the screen
+            scoreLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            scoreLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+            
+            // more constraints to be added here!
+            ])
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
