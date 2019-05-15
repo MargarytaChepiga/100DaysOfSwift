84baa92a769aeb9099a50661a31a85c39682bb2a
MargarytaChepiga <chepigamargaryta@gmail.com>
2019-05-15 00:02:08 -0400

    Completed Project10

---
+++ Project10-Names-to-Faces/Project10/Person.swift
@@ -0,0 +1,23 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by margaret on 2019-05-14.
+//  Copyright © 2019 margaret. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        
+        self.name = name
+        self.image = image
+        
+    }
+    
+}
--- Project10-Names-to-Faces/Project10/ViewController.swift
+++ Project10-Names-to-Faces/Project10/ViewController.swift
@@ -8,15 +8,21 @@
 
 import UIKit
 
-class ViewController: UICollectionViewController {
+// tell Swift you promise your class supports all the functionality required by the two protocols UIImagePickerControllerDelegate and UINavigationControllerDelegate
+class ViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    var people = [Person]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
+        
     }
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return people.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -25,8 +31,93 @@ class ViewController: UICollectionViewController {
             fatalError("Unable to dequeue PersonCell.")
         }
         
+        let person = people[indexPath.item]
+        
+        cell.name.text = person.name
+        
+        let path = getDocumentsDirectory().appendingPathComponent(person.image)
+        cell.imageView.image = UIImage(contentsOfFile: path.path)
+        
+        cell.imageView.layer.borderColor = UIColor(white: 0, alpha: 0.3).cgColor
+        cell.imageView.layer.borderWidth = 2
+        cell.imageView.layer.cornerRadius = 3
+        cell.layer.cornerRadius = 7
+        
         // if we're still here it means we got a PersonCell, so we can return it
         return cell
+    }
+    
+    @objc func addNewPerson() {
+        
+        let picker = UIImagePickerController()
+        // allows the user to crop the picture they select
+        picker.allowsEditing = true
+        print("HERE  ")
+        print(UIImagePickerController.isSourceTypeAvailable(.camera))
+        if UIImagePickerController.isSourceTypeAvailable(.camera) == true {
+            picker.sourceType = .camera
+        }
+        //picker.sourceType = .camera
+        // you'll need to conform not only to the UIImagePickerControllerDelegate protocol, but also the UINavigationControllerDelegate protocol
+        picker.delegate = self
+        present(picker, animated: true)
+        
+    }
+    
+    //  delegate method which returns when the user selected an image
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        guard let image = info[.editedImage] as? UIImage else { return }
+        
+        let imageName = UUID().uuidString
+        let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
+        
+        if let jpegData = image.jpegData(compressionQuality: 0.8) {
+            try? jpegData.write(to: imagePath)
+        }
+        
+        
+        let person = Person(name: "Unknown", image: imageName)
+        people.append(person)
+        collectionView.reloadData()
+        
+        dismiss(animated: true)
+        
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+        
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        let person = people[indexPath.item]
+        
+        let renameAlert = UIAlertController(title: "Remane the contact", message: nil, preferredStyle: .alert)
+        renameAlert.addTextField()
+        renameAlert.addAction(UIAlertAction(title: "OK", style: .default) { [weak self, weak renameAlert] _ in
+            guard let newName = renameAlert?.textFields?[0].text else { return }
+            person.name = newName
+            
+            self?.collectionView.reloadData()
+            
+        })
+        renameAlert.addAction(UIAlertAction(title: "Cancel", style: .default))
+        
+        let alert = UIAlertController(title: "Do you want to rename or delete a contact?", message: nil, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Rename", style: .default, handler: {
+            [weak self] _ in
+            self?.present(renameAlert, animated: true)
+        }))
+        alert.addAction(UIAlertAction(title: "Delete", style: .default, handler: { [weak self] _ in
+            self?.people.remove(at: indexPath.row)
+              self?.collectionView.reloadData()
+        }))
+        
+        present(alert, animated: true)
     }
 
 }
