7a6cdaab3ba240f2011a58128f422032221e79a1
MargarytaChepiga <chepigamargaryta@gmail.com>
2019-05-15 22:03:24 -0400

    Added score property and it's observer to keep track of the users score

--- Project11-Peggle/Project11/GameScene.swift
+++ Project11-Peggle/Project11/GameScene.swift
@@ -10,6 +10,14 @@ import SpriteKit
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
     
+    var scoreLabel: SKLabelNode!
+    
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    
     // equivalent of SpriteKit's viewDidLoad() method
     override func didMove(to view: SKView) {
         
@@ -36,6 +44,13 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         makeBouncer(at: CGPoint(x: 512, y: 0))
         makeBouncer(at: CGPoint(x: 768, y: 0))
         makeBouncer(at: CGPoint(x: 1024, y: 0))
+        
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.text = "Score: 0"
+        scoreLabel.horizontalAlignmentMode = .right
+        scoreLabel.position = CGPoint(x: 980, y: 700)
+        addChild(scoreLabel)
+        
     }
     
     // This method gets called (in UIKit and SpriteKit) whenever someone starts touching their device
@@ -102,8 +117,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     func collisionBetween(ball: SKNode, object: SKNode) {
         if object.name == "good" {
             destroy(ball: ball)
+            score += 1
         } else if object.name == "bad" {
             destroy(ball: ball)
+            score -= 1
         }
     }
     
